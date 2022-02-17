d35bb4bf2b28b833d319e7b50e24c73b9906cd72
Arjun Kavungal <arjunkavungal@gmail.com>
2022-02-16 21:17:28 -0500

    import

--- SmartCity.java
+++ SmartCity.java
@@ -1,5 +1,8 @@
+import com.google.gson.JsonElement;
 public class SmartCity {
     public static void main(String args[]) {
         System.out.println("Smart City");
+        //Tourism module
+
     }
 }
