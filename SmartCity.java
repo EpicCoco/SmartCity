46a606c157d71f94462fbc758d0ed672615702fe
Arjun Kavungal <arjunkavungal@gmail.com>
2022-02-17 13:24:12 -0500

    add hotel names

--- SmartCity.java
+++ SmartCity.java
@@ -25,19 +25,24 @@ public class SmartCity {
         URL url = new URL("https://api.geoapify.com/v2/places?categories=accommodation.hotel&filter=rect:-83.5565068267818,34.08940116555951,-83.18829317321723,33.79675168842013&limit=20&apiKey=a305ad8ec2e34a569c192df2bba27be2");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         StringBuilder result = new StringBuilder();
-      conn.setRequestMethod("GET");
-      try (BufferedReader reader = new BufferedReader(
-                  new InputStreamReader(conn.getInputStream()))) {
-          for (String line; (line = reader.readLine()) != null; ) {
-              result.append(line);
-              
-          }
-          String s = result.toString();
-          JsonParser jsonParser = new JsonParser();
-JsonObject jo = (JsonObject)jsonParser.parse(s);
-System.out.println(jo);
-JsonArray status = jo.getAsJsonArray("features");
-System.out.println(status);
+        conn.setRequestMethod("GET");
+        try (BufferedReader reader = new BufferedReader(
+            new InputStreamReader(conn.getInputStream()))) {
+            for (String line; (line = reader.readLine()) != null; ) {
+                result.append(line);   
+            }
+            String s = result.toString();
+            JsonParser jsonParser = new JsonParser();
+            JsonObject jo = (JsonObject)jsonParser.parse(s);
+            JsonArray status = jo.getAsJsonArray("features");
+            for (int i = 0; i < status.size(); i++){
+                JsonElement hotel = status.get(i);
+                JsonObject j = hotel.getAsJsonObject();
+                JsonObject properties = j.getAsJsonObject("properties");
+                JsonPrimitive addr = properties.getAsJsonPrimitive("address_line2");
+                System.out.println(addr);
+            }
+
         }
     }
 }
