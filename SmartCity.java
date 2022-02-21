2a8c2c4328de55d511c7ef895019b3946027d594
Arjun Kavungal <arjunkavungal@gmail.com>
2022-02-21 00:31:31 -0500

    add attraction names

--- SmartCity.java
+++ SmartCity.java
@@ -1,5 +1,7 @@
 import org.jsoup.Jsoup;
+import java.util.LinkedList;
 import org.jsoup.nodes.Document;
+import java.util.stream.Collectors;
 import java.net.URL;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -18,10 +20,12 @@ import java.io.*;
 import java.util.*;
 import org.json.simple.parser.JSONParser;
 import com.google.gson.*;
+import com.careerjet.webservice.api.Client;
 public class SmartCity {
     public static void main(String args[]) throws Exception{
         System.out.println("Smart City");
         //Tourism module
+        //get hotel
         URL url = new URL("https://api.geoapify.com/v2/places?categories=accommodation.hotel&filter=rect:-83.5565068267818,34.08940116555951,-83.18829317321723,33.79675168842013&limit=20&apiKey=a305ad8ec2e34a569c192df2bba27be2");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         StringBuilder result = new StringBuilder();
@@ -39,10 +43,52 @@ public class SmartCity {
                 JsonElement hotel = status.get(i);
                 JsonObject j = hotel.getAsJsonObject();
                 JsonObject properties = j.getAsJsonObject("properties");
+                JsonPrimitive hotelName = properties.getAsJsonPrimitive("name");
+                if (hotelName == null) {
+                    hotelName = properties.getAsJsonPrimitive("address_line1");
+                }
                 JsonPrimitive addr = properties.getAsJsonPrimitive("address_line2");
-                System.out.println(addr);
+                System.out.println(hotelName + "\n" + addr);
             }
 
         }
+        URL url2 = new URL("https://api.opentripmap.com/0.1/en/places/radius?radius=20000&lon=-83.357604&lat=33.9519&apikey=5ae2e3f221c38a28845f05b634ee2da680014417a5342d1496cdc86a");
+        HttpURLConnection conn2 = (HttpURLConnection) url2.openConnection();
+        StringBuilder result2 = new StringBuilder();
+        conn2.setRequestMethod("GET");
+        try (BufferedReader reader2 = new BufferedReader(
+            new InputStreamReader(conn2.getInputStream()))) {
+                for (String line2; (line2 = reader2.readLine()) != null; ) {
+                    result2.append(line2); 
+                }
+                String s2 = result2.toString();
+                JsonParser jsonParser = new JsonParser();
+                JsonObject root = (JsonObject)jsonParser.parse(s2);
+                JsonArray features = root.getAsJsonArray("features");
+                int count=0;
+                LinkedList<String> types = new LinkedList<String>();
+                
+                for (int i = 0; i < features.size(); i++) {
+                    JsonElement place = features.get(i);
+                    JsonObject placeObject = place.getAsJsonObject();
+                    JsonObject properties = placeObject.getAsJsonObject("properties");
+                    System.out.println(properties.getAsJsonPrimitive("name").getAsString() + properties.getAsJsonPrimitive("kinds").getAsString());
+                    
+                    types.addLast(properties.getAsJsonPrimitive("kinds").getAsString());
+                    
+                    for (String j:types){
+                        if (j.equals("cemeteries,historic,burial_places,interesting_places")){
+                            count += 1;
+                        }
+                    }
+                    
+                    
+                }
+                List<String> Unique = types.stream().distinct().collect(Collectors.toList());
+                for (String s: Unique) {
+                    System.out.println(s + ": " + Collections.frequency(types, s));
+                }
+        }
+        
     }
 }
